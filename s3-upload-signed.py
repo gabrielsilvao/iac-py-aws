f9291798779368f1b7f5ed4e56c66ba0da6c2c7f
root <loktwo@outlook.com>
2021-06-24 15:48:13 +0000

    codes v3

---
+++ s3-upload-signed.py
@@ -0,0 +1,28 @@
+import boto3
+import botocore
+import argparse
+import os
+
+def create_bucket(args):
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket=args.name)
+
+def upload_file(args):
+    s3 = boto3.client('s3')
+    s3.upload_file(args.file, args.name, args.file)
+
+def sign_url(args):
+    s3 = boto3.client('s3')
+    signed_url = s3.generate_presigned_url(ClientMethod="get_object", ExpiresIn=3600,
+                                            Params={args.sign-file, "Key": args.file})
+    print(signed_url)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create a bucket s3")
+    parser.add_argument("-n", "--name", metavar="NAME", help="Name of the bucket")
+    parser.add_argument("-f", "--file", metavar="FILE", help="Name of the file")
+    parser.add_argument("-s", "--sign-file", metavar="SIGN-FILE", help="Name of the file")
+    args = parser.parse_args()
+    create_bucket(args)
+    upload_file(args)
+    sign_url(args)
