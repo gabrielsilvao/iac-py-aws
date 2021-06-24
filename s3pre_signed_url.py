b05d0fd0c30fcad98cf5e875d797252c35823810
Gabriel <loktwo@outlook.com>
2021-06-24 10:35:55 -0300

    codes v2

---
+++ s3pre_signed_url.py
@@ -0,0 +1,7 @@
+import boto3
+
+share_url = s3.generate_presigned_url(ClientMethod="get_object",
+                                        ExpiresIn=3600,
+                                        Params="Bucket": "my-bucket-test-sdk12-12-31-2", "key": "create_bucket.py")
+
+print(share_url)
